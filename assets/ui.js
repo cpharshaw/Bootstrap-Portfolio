ccff40ab464b6652b7bde932a786316481533d9e
cpharshaw <cpharshaw+git@gmail.com>
2019-02-26 15:27:13 -0500

    done for now

---
+++ assets/ui.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+
+    $("#portfolioNav").on('click', function () {
+        $("#secondBackground").css({
+            "background": "rgba(19, 19, 25, 0.875",
+            "transition": "background-color 0.75s "
+        });
+
+        $("#about").fadeOut(200);
+        $("#contact").fadeOut(200);
+        $("#landing").fadeOut(200);
+
+        $("#portfolio").fadeIn(1000);
+    });
+
+
+
+    $("#aboutNav").on('click', function () {
+        $("#secondBackground").css({
+            "background": "none",
+            "transition": "background 0.75s "
+        });
+
+        $("#contact").fadeOut(200);
+        $("#portfolio").fadeOut(200);
+        $("#landing").fadeOut(200);
+
+        $("#about").fadeIn(750);
+    });
+
+
+    $("#contactNav").on('click', function () {
+        $("#secondBackground").css({
+            "background": "none",
+            "transition": "background 0.75s"
+        });
+
+        $("#about").fadeOut(200);
+        $("#portfolio").fadeOut(200);
+        $("#landing").fadeOut(200);
+
+        $("#contact").fadeIn(500);
+    });
+
+
+
+    $("#myName").on('click', function () {
+        $("#secondBackground").css({
+            "background": "none",
+            "transition": "background 0.75s"
+        });
+
+        $("#about").fadeOut(200);
+        $("#portfolio").fadeOut(200);
+        $("#contact").fadeOut(200);
+
+        $("#landing").fadeIn(500);
+    });
+    
+
+
+
+
+
+});
